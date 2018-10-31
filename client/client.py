7219b92c8786b01d781627433faf5fcd56642760
Randy Pitcher <randypitcherii@gmail.com>
2018-10-31 04:10:43 -0500

    cleaned up logging and printing in client

--- client/client.py
+++ client/client.py
@@ -3,22 +3,24 @@ import settings
 import logging, time
 import slashmap
 
+def printAnalysis(analysis):
+    print("\n\n")
+    [print(f"label: {label['Name']}, conf: {label['Confidence']}") for label in analysis]
+    print("\n\n")
+
 def main():
     logging.basicConfig(level=logging.INFO)
     logging.info("STARTING CLIENT...")
 
     # loop
     while True:
-        logging.info("SNAPPING PICTURE...")
         camera.takePicture()
-        logging.info(f"PICTURE SAVED TO {settings.IMG_PATH}...") 
 
         s3_filename = slashmap.getS3Filename()
-        logging.info(f"Uploading to S3 {s3_filename}...")
         slashmap.uploadToS3(settings.IMG_PATH, s3_filename)
-        logging.info(f"Getting analysis for {s3_filename}...")
+        logging.info(f"Getting analysis for {s3_filename}")
         analysis = slashmap.getAnalysis(s3_filename)
-        [print(f"label: {label['Name']}, conf: {label['Confidence']}") for label in analysis]
+        printAnalysis(analysis)
 
         time.sleep(settings.LOOP_TIMEOUT)
 
